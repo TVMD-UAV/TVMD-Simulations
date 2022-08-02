3c180c4fc1b9b5cb55883229a5addbe5ea46bf6a
sciyen <a885566885566@gmail.com>
2022-08-03 00:32:02 +0800

    Add mode switching feature
    Using state machine to control the transition of different flight mode.

--- src/demo_ctrl/navigator/lib/Controller/JoystickController.h
+++ src/demo_ctrl/navigator/lib/Controller/JoystickController.h
@@ -3,10 +3,26 @@
 
 #include "Controller.h"
 
-enum FlightMode { Hover = 0, FixedWing };
+#define FixWingModeServoAngle 250
+#define HoverModeServoAngle 127
+
+#define HoverMotorSpeed 127
+#define FixWingMotorSpeed 230
+#define TransitionMotorSpeed 150
+
+#define TimeForTransition 5000  // in milliseconds
+
+enum FlightMode {
+    Hover = 0,
+    FixedWing,
+    TransitionToHover,
+    TransitionToFixedWing
+};
 class JoystickController : public Controller
 {
 public:
+    enum InputState { Buf1, Buf2 };
+    InputState mirror_input;
     JoystickController(Commander *commander);
 
     void generate_commands() override;
@@ -18,7 +34,16 @@ protected:
 
     FlightMode _flight_mode;
 
-    int _inputs[6];
+    // 0: hover mode
+    // 1: fixed wing
+    float _transition_state;
+
+    int _inputs1[6];
+    int _inputs2[6];
+
+    float calc_transition_servo_angle();
+
+    uint32_t _transistion_start_time;
 };
 
 #endif
