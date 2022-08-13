21e2d735949dee6fb95d90926a83fa34cc6c54b3
sciyen <a885566885566@gmail.com>
2022-08-13 21:18:59 +0800

    Bump with clang-format

--- src/demo_ctrl/agent_ctrl/lib/IsrLed/IsrLed.h
+++ src/demo_ctrl/agent_ctrl/lib/IsrLed/IsrLed.h
@@ -4,31 +4,31 @@
 #include "Arduino.h"
 
 #define LED_TYPE uint8_t
-#define LED_LEVEL 16
+#define LED_LEVEL 15
 
-class IsrLed{
-    public:
-        IsrLed();
+class IsrLed {
+public:
+  IsrLed();
 
-        void init();
+  void init();
 
-        /* Set the leds via HSL color space.
-        * @Params:
-        *      H: Hue, 0~360
-        *      S: Saturation, 0~1
-        *      V: Value, 0~1, (Set to 1 for white, 0 for black)
-        */
-        void set_hsl(int H, float S, float L);
+  /* Set the leds via HSL color space.
+   * @Params:
+   *      H: Hue, 0~360
+   *      S: Saturation, 0~1
+   *      V: Value, 0~1, (Set to 1 for white, 0 for black)
+   */
+  void set_hsl(int H, float S, float L);
 
-        static void update();
+  static void update();
 
-    protected:
-        static volatile LED_TYPE _leds[3];
+protected:
+  static volatile LED_TYPE _leds[3];
 
-        float hue_to_rgb(float v1, float v2, float vH);
+  float hue_to_rgb(float v1, float v2, float vH);
 
-        void ISR_enable();
-        
-        void ISR_disable();
+  void ISR_enable();
+
+  void ISR_disable();
 };
 #endif
--- src/demo_ctrl/agent_ctrl/lib/Receiver/Receiver.h
+++ src/demo_ctrl/agent_ctrl/lib/Receiver/Receiver.h
@@ -10,42 +10,42 @@
 #define DATA_LEN 8
 #define DUP_DATA_LEN 3
 
-#define MY_ID 10
+#define MY_ID 8
 
-struct AgentCommands_t{
-    uint8_t id;
-    uint8_t upper_motor, bottom_motor;
-    uint8_t center_servo, outer_servo;
-    uint8_t h, s, l;
+struct AgentCommands_t {
+  uint8_t id;
+  uint8_t upper_motor, bottom_motor;
+  uint8_t center_servo, outer_servo;
+  uint8_t h, s, l;
 };
 
-union UartPacket_t{
-    uint8_t raw[DATA_LEN];
-    AgentCommands_t data;
+union UartPacket_t {
+  uint8_t raw[DATA_LEN];
+  AgentCommands_t data;
 };
 
-class Receiver{
+class Receiver {
 public:
-    Receiver();
+  Receiver();
 
-    void init();
+  void init();
 
-    // True if new packet arrived
-    bool available();
+  // True if new packet arrived
+  bool available();
 
-    AgentCommands_t read();
+  AgentCommands_t read();
 
-    void update();
+  void update();
 
 protected:
-    bool new_data_flag;
-    bool new_packet_flag;
-    RingBuf<uint8_t, DUP_DATA_LEN * DATA_LEN> _raw_buf;
-    UartPacket_t _packet;
+  bool new_data_flag;
+  bool new_packet_flag;
+  RingBuf<uint8_t, DUP_DATA_LEN * DATA_LEN> _raw_buf;
+  UartPacket_t _packet;
 
-    void check_for_new_data();
+  void check_for_new_data();
 
-    void flush();
+  void flush();
 };
 
 #endif
